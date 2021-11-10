c615e66b0e6b64a20def80713797a51f3df6539a
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-10 18:42:49 -0500

    Index and helper skeleton structure

--- helper/helper.js
+++ helper/helper.js
@@ -1,3 +1,7 @@
+// Dependencies
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
 function alldeps(){
     return '';
 }
@@ -10,4 +14,50 @@ function allemployees(){
     return '';
 }
 
-module.exports = {alldeps,allroles,allemployees};
+function addDep(){
+
+}
+
+function addRole(){
+
+}
+
+function addEmp(){
+
+}
+
+function updateEmployeeRole(){
+
+}
+
+function UpdateEmployeeMang(){
+
+}
+
+function getEmpByMang(){
+
+}
+
+function getEmpByDep(){
+
+}
+
+function deleteDep(){
+
+}
+
+function deleteRole(){
+
+}
+
+function deleteEmp(){
+
+}
+
+function getTotalBudget(){
+
+}
+
+module.exports = {alldeps,allroles,allemployees,addDep,addRole,addEmp,
+                     updateEmployeeRole,UpdateEmployeeMang,getEmpByMang,
+                     getEmpByDep,deleteDep,deleteRole,deleteEmp,getTotalBudget};
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Dependencies 
 const inq = require("inquirer");
-const mysql = require('mysql2');
-const cTable = require('console.table');
 const helper = require("./helper/helper");
 
 // Options asked at start of app
@@ -34,6 +32,37 @@ function appStartOptions(){
     ]).then(opt=>{
 
         console.log(opt);
+        if(opt=='1.) View all the Departments'){
+            helper.alldeps();
+        }else if(opt=='2.) View all roles'){
+            helper.allroles();
+        }else if(opt=='3.) View all employees'){
+            helper.allemployees();
+        }else if(opt=='4.) Add a department'){
+            helper.addDep()
+        }else if(opt=='5.) Add a role'){
+            helper.addRole();
+        }else if(opt=='6.) Add an employee'){
+            helper.addEmp();
+        }else if(opt=='7.) Update an employee role'){
+            helper.updateEmployeeRole();
+        }else if(opt=='8.) Update employee managers'){
+            helper.UpdateEmployeeMang()
+        }else if(opt=='9.) View employees by manager'){
+            helper.getEmpByMang()
+        }else if(opt=='10.) View employees by department'){
+            helper.getEmpByDep();
+        }else if(opt=='11.) Delete a department'){
+            helper.deleteDep();
+        }else if(opt=='12.) Delete a role'){
+            helper.deleteRole();
+        }else if(opt=='13.) Delete a employee'){
+            helper.deleteEmp();
+        }else if(opt=='14.) View the total utilized budget of a department'){
+            helper.getTotalBudget();
+        }else{
+
+        }
 
     });
 }
